3ef2d65a4e694b2308e04f4f9101210b01ab56da
Anne <anne.k.plummer@gmail.com>
2020-09-26 23:13:37 -0500

    Thought models

--- models/Thought.js
+++ models/Thought.js
@@ -1,9 +1,42 @@
+const { Schema, model } = require('mongoose');
+const moment = require('moment');
+const reactionsSchema = require('./Reaction');
+
+const ThoughtSchema = new Schema(
+    {
 // thoughtText : String, Required, must be between 1-280 characters
-
+        thoughtText: {
+            type: String,
+            required: true,
+            minlength: 1,
+            maxlength: 280,
+        },
 // createdAt : Date, Set default value to the current timestamp, use moment in getter method to format timestamp on query
-
+        createdAt: {
+            type: Date,
+            default: Date.now,
+            get: (timestamp) => moment(timestamp).format('MM Do, YYYY [at] hh:mm a'),
+        },
 // username (user that created the thought): String, Required
-
+        username: {
+            type: String,
+            required: true,
+        },
 // reactions (replies) : array of nested documents created with the reactionSchema
-
+        reactions: [reactionSchema],
+    },
+    {
+        toJSON: {
+            getters: true,
+        },
+        id: false,
+    }
+);
 // Schema Settings : create a virtual called 'reactionCount' that retrives the length of the thought's reactions array field on query
+    ThoughtSchema.virutal('reactionCount').get(function(){
+        return this.reactions.length;
+    });
+
+const Thought =  model('Thought', thoughtSchema);
+
+module.exports = Thought;
--- models/User.js
+++ models/User.js
@@ -1,7 +1,6 @@
 const { Schema, model } = require('mongoose');
 
 const UsernameSchema = new Schema(
-// username: String, Unique, Required, Trimmed
     {
         username: {
             type: String,
@@ -9,29 +8,24 @@ const UsernameSchema = new Schema(
             required: true,
             trim: true,
         },
-
-    // email: String, Required, Unique, must match valid email address (Mongoose matching validation)
         email: {
             type: String,
             unique: true,
             required: true,
             match: [/^([\w-\.]+@([\w-]+\.)+[\w-]{2,4})?$/, 'This must be a valid email!']
         },
-    // thoughts: array of _id values referencing the Thought model
         thoughts: [
             {
                 type: Schema.Types.ObjectId,
                 ref: 'Thought',
             },
         ],
-    // friends: array of _id values referencing the User model (self-reference)
         friends: [
             {
                 type: Schema.Types.ObjectId,
                 ref: 'User'
             },
         ],
-
     },
     {
         toJSON: {
